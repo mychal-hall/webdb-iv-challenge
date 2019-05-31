56fbd0ec29d4357a6b3e5790270955d190105587
Mychal <hallmychal@gmail.com>
2019-05-31 18:13:28 -0500

    GET dish by id is up

--- routes/dishesModel.js
+++ routes/dishesModel.js
@@ -4,9 +4,23 @@ const config = require("../knexfile.js");
 const db = knex(config.development);
 
 module.exports = {
-  findDishes
+  findDishes,
+  findDishById,
+  insertDish
 };
 
 function findDishes() {
   return db("dishes");
+}
+
+function findDishById(id) {
+  return db
+    .select("dishes.id", "dishes.name as dish", "recipes.name as recipe")
+    .from("dishes")
+    .leftJoin("recipes", "dishes.id", "recipes.dish_id")
+    .where("dishes.id", Number(id));
+}
+
+function insertDish(dish) {
+  
 }
--- routes/dishesRouter.js
+++ routes/dishesRouter.js
@@ -15,8 +15,32 @@ router.get("/", async (req, res) => {
       .json({ messages: "The dishes are unreachable, is the server on?" });
   }
 });
-// Lists one dish with a specific ID -- GET /api/dishes/:id
 
+// Lists one dish with a specific ID -- GET /api/dishes/:id
+router.get("/:id", async (req, res) => {
+  try {
+    const dish = await Dishes.findDishById(req.params.id);
+    if (dish.length) {
+      const recipesRelated = dish.map(item => {
+        const recipe = { name: item.recipe };
+        return recipe;
+      });
+      const dishRelated = {
+        id: dish[0].id,
+        name: dish[0].dish,
+        recipes: recipesRelated
+      };
+      res.status(200).json(dishRelated);
+    } else {
+      res.status(404).json({ message: "There is no dish with that ID!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "The dish could not be found. Is the server actually on!?"
+    });
+  }
+});
 // Adds a new dish to the database -- POST /api/dishes
 
 module.exports = router;
